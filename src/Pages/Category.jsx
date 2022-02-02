b3971ca057a81008ae9818c2f2ac1fa18446384b
LeonieNollevalle <leonie.nollevalle@gmail.com>
2022-02-02 20:36:45 +0100

    fetch route get + ajout des link et params

--- src/Pages/Category.jsx
+++ src/Pages/Category.jsx
@@ -1,7 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router';
+import axios from 'axios';
 
 const Category = () => {
-  return <div />;
+  const { id } = useParams();
+  const [adddata, setData] = useState();
+  useEffect(() => {
+    axios
+      .get(`http://localhost:4000/projet/categorie/${id}`)
+      .then((res) => setData(res.data))
+      .catch((error) => console.log(error));
+  }, []);
+
+  console.log(adddata);
+
+  return (
+    <div>
+      {adddata.map((add) => {
+        return <img src={add.image_url} alt={add.title} />;
+      })}
+    </div>
+  );
 };
 
 export default Category;
